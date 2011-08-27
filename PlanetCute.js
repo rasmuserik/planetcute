3a770873269ee5b4668b128786b5f3af1f1ae724
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-08-27 19:21:13 +0200

    ...

--- PlanetCute.js
+++ PlanetCute.js
@@ -1,7 +1,104 @@
 PlanetCute = function(mapDesc) {
+    var tileset = {
+      "+": {landscape: "Stone Block", tileHeight: 1},
+      "o": {landscape: "Brown Block", tileHeight: 1},
+      "p": {landscape: "Plain Block", tileHeight: 1},
+      "d": {landscape: "Dirt Block", tileHeight: 1},
+      " ": {landscape: "Grass Block", tileHeight: 1},
+      "~": {landscape: "Water Block", tileHeight: 1},
+      "=": {landscape: "Wood Block", tileHeight: 1},
+      "#": {landscape: "Wall Block", tileHeight: 1},
+      "A": {landscape: "Ramp North", tileHeight: 1},
+      ">": {landscape: "Ramp East", tileHeight: 1},
+      "<": {landscape: "Ramp West", tileHeight: 1},
+      "V": {landscape: "Ramp South", tileHeight: 1}
+    };
+
+    
+    function mapParse(mapDesc) {
+        var map = {};
+        for(y=0;y<mapDesc.length;++y) {
+            for(x=0;x<mapDesc[y].length/2;++x) {
+                var result = Object.create(tileset[mapDesc[y][x*2+1]]);
+                result.x = x;
+                result.y = y;
+                result.h = +mapDesc[y][x*2];
+                map[x+','+y] = result;
+            }
+        }
+        return map;
+    }
+    map = mapParse(mapDesc);
+
+    function mapLookup(x,y) {
+        return map[x+','+y];
+    }
+
+    var imgs;
+
+    function drawMap(x0,y0) {
+        if(!imgs) {
+            imgs = [];
+            for(var i=0;i<49;++i) {
+                var t = $('<img>');
+                $('#sprites').append(t);
+                imgs.push(t[0]);
+            }
+        }
+        var screenWidth = 400;
+        var imgWidth = Math.round(screenWidth/5.5);
+        var imgHeight = Math.round(1.7*imgWidth);
+        var flatHeight = Math.round(.8*imgWidth);
+        var groundHeight = Math.round(.4*imgWidth);
+        var x, y;
+        var dx = x0 - Math.round(x0);
+        var x0 = Math.round(x0);
+
+        var deltay = Math.round(screenWidth/2 - 1.5*imgWidth - map[x0+','+y0].h*groundHeight);
+        var deltax = Math.round((screenWidth-imgWidth)/2 - dx * imgWidth);
+
+        html = [];
+        var stylemid = 'px;width:'+imgWidth+'px;height:'+imgHeight+'px;left:';
+        for(x=-3;x<=3;++x) {
+            for(y=-3;y<=3;++y) {
+                var obj = map[(x0+x) + ',' + (y0+y)];
+                var file = "PlanetCute/" + obj.landscape + ".png";
+                var imx = x * imgWidth + deltax;
+                var imy = y * flatHeight + obj.h * groundHeight + deltay;
+                html.push('<img src="');
+                html.push(file);
+                html.push('" style="top:');
+                html.push(imy);
+                html.push(stylemid);
+                html.push(imx);
+                html.push('px">');
+            }
+        }
+        $("#sprites").html(html.join(''));
+    }
+
+    pos = 3;
+    function draw() {
+        drawMap(pos,4);
+        pos+=.05;
+        setTimeout(draw,50);
+    }
+    draw();
+
+
+
+    /*
+    function toY(y, h) {
+            sprite.h = +mapDesc[y][x*2];
+            sprite.layer(y*2);
+            sprite.resize(imgWidth, imgHeight);
+            sprite.moveTo(toX(x), toY(y, sprite.h));
+            }
+        }
+    }
 
     var x,y;
-    var map = [];
+    var pmap = [];
     var tiles = {
       "+": "Stone Block",
       "o": "Brown Block",
@@ -16,7 +113,7 @@ PlanetCute = function(mapDesc) {
       "<": "Ramp West",
       "V": "Ramp South"
     };
-    var imgWidth = 320/5.5;
+    var imgWidth = 400/5.5;
     var imgHeight = 1.7*imgWidth;
     var flatHeight = .8*imgWidth;
     var groundHeight = .4*imgWidth;
@@ -24,11 +121,11 @@ PlanetCute = function(mapDesc) {
         return x * imgWidth - imgWidth *.8;
     }
     function toY(y, h) {
-        return y * flatHeight + h * groundHeight - 90;
+        return y * flatHeight + h * groundHeight - 120;
     }
     for(y=0;y<mapDesc.length;++y) {
         var row = [];
-        map.push(row);
+        pmap.push(row);
         for(x=0;x<mapDesc[y].length/2;++x) {
             var type = tiles[mapDesc[y][x*2+1]];
             type = "PlanetCute/" + type + ".png";
@@ -47,7 +144,7 @@ PlanetCute = function(mapDesc) {
         return toX(x);
     }
     function getY(x, y) {
-        return toY(y, map[y][x].h - 1);
+        return toY(y, pmap[y][x].h - 1);
     }
 
     function obj(name, x, y) {
@@ -67,6 +164,7 @@ PlanetCute = function(mapDesc) {
     obj("Tree Tall", 0,5);
     //obj("Tree Tall", 10,1);
     obj("Tree Short", 2,2);
+    */
 };
 
 Q.setMain(function() {
