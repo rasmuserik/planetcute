04b799177f4131f28e6ccb3c6139b2fc97b75034
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-08-28 17:07:02 +0200

    ...

--- js/PlanetCute.js
+++ js/PlanetCute.js
@@ -1,6 +1,6 @@
 // TODO: animate containing div rather than individual imgs for performance
 
-// 10x7 viewport
+// 7x6 viewport
 //
 
 PlanetCute = function(mapDesc) {
@@ -40,9 +40,9 @@ PlanetCute = function(mapDesc) {
           "c": "Roof-South-East",
         };
 
-        for(x=0;x<10;++x) {
-            for(y=0;y<7;++y) {
-                map[y*10+x] = {
+        for(x=0;x<7;++x) {
+            for(y=0;y<6;++y) {
+                map[y*7+x] = {
                     item: objset[mapDesc[y][x*3]],
                     h: +mapDesc[y][x*3+1],
                     tile: tileset[mapDesc[y][x*3+2]],
@@ -55,8 +55,8 @@ PlanetCute = function(mapDesc) {
 
     var scrWidth = $('#viewport').width();
     var scrHeight = $('#viewport').height();
-    var imWidth = Math.floor(scrWidth / 10);
-    var imHeight = Math.floor(scrHeight / 3.2/ 5) * 5;
+    var imWidth = Math.floor(scrWidth / 7);
+    var imHeight = Math.floor(scrHeight / 2.4/ 5) * 5;
 
     // TILE    OBJ
     //
@@ -81,7 +81,7 @@ PlanetCute = function(mapDesc) {
                 html.push(defaultstyle);
                 html.push('left:' + imWidth*obj.x + "px;");
                 html.push('z-index:' + obj.y*2 + ";");
-                html.push('top:' + (imHeight*.4*obj.y + (6-obj.h) * imHeight * .1) + "px;");
+                html.push('top:' + (imHeight*.4*obj.y + (3-obj.h) * imHeight * .1) + "px;");
                 html.push('">');
             }
             if(obj.item) {
@@ -90,57 +90,38 @@ PlanetCute = function(mapDesc) {
                 html.push(defaultstyle);
                 html.push('left:' + imWidth*obj.x + "px;");
                 html.push('z-index:' + obj.y*2 + ";");
-                html.push('top:' + (imHeight*.4*obj.y + (2-obj.h) * imHeight * .1) + "px;");
+                html.push('top:' + (imHeight*.4*obj.y + (-1-obj.h) * imHeight * .1) + "px;");
                 html.push('">');
             }
-            console.log(obj);
         }
         $("#viewport").html(html.join(""));
+        console.log(map);
     }
     function imgY(x,y) {
-        return imHeight*.4*y + (2-map[x+10*y].h) * imHeight * .1;
+        return imHeight*.4*y + (-1-map[x+7*y].h) * imHeight * .1;
     }
 
     mapParse(mapDesc);
     drawMap();
-    $("#viewport").append($('<img src="imgs/obj/Character-Princess-Girl.png" style="position:absolute;top:' + imgY(5,4) + 'px;left:' + 5 * imWidth + 'px;z-index:' + 4*2 + ';width:' + imWidth + 'px;height:' + imHeight + 'px;">'));
+    x=3; y = 3;
+    $("#viewport").append($('<img src="imgs/obj/Character-Princess-Girl.png" style="position:absolute;top:' + imgY(x,y) + 'px;left:' + x * imWidth + 'px;z-index:' + y*2 + ';width:' + imWidth + 'px;height:' + imHeight + 'px;">'));
 };
 
 Q.setMain(function() {
     PlanetCute([
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 ",
-    " 5  5  5  5  5  5  5  5  5  5 "
+    " 5  5  5  5  5  5  6  ",
+    " 5  5  5  5  5  5  6  ",
+    " 5  5  5  5  5  5  6  ",
+    " 5  5  5  5  5  5  6  ",
+    " 5  5  5  5  5  5  6  ",
+    " 5  5  5  5  5  5  6  "
     ]);
     PlanetCute([
-    " 9  8  7  6  5  5  5  5  5  5 ",
-    " 7  7  6  6  5  5  7  7  7  7 ",
-    " 6  6  5  5  5  5  7  9  9  7 ",
-    " 5  5  5  5  6  7  8  9  9  7 ",
-    " 4  4  4  5  5  5  7  7  7  7 ",
-    " 2  3  3  4  4  5  5  5  5  5 ",
-    " 0  1  2  3  4  5  5  5  5  5 "
-    ]);
-    PlanetCute([
-    " 9+ 5  5  5  5  5  4d 4d 4d 4d",
-    " 7# 5  5  5  5  5  4d 2o 2o 2o",
-    " 7+ 7+ 7+ 5  5  5  4d 2o 0+ 0+",
-    "o9+ 8+ 7+ 6# 5  4d 3d 2o 1o 0+",
-    " 7+ 7+ 7+ 5  5  5  4d 2o 0+ 0+",
-    " 5  5  5  5  5  5  4d 2o 2o 2o",
-    " 5  5  5  5  5  5  4d 4d 4d 0+"
-    ]);
-    PlanetCute([
-    " 5q 5q 5qt5 t5 t5 t5 t5 t5 t5 ",
-    "a9qs9qd9q 5  5  5  5  5  5 t5 ",
-    "z9Sx9Dc9ST5  5  4d 3d 3d*5 t5 ",
-    "t5  5d 5  5  5  5  5  5  5 t5 ",
-    " 5d 5d 5d 6+ 7+ 5 C5  5q 5 t5 ",
-    " 5~ 5~ 5~ 5~ 7= 5~ 5~ 5~ 5~ 5~",
-    " 5  5 o5 r5  7+ 6+ 5d 5d 5d 5d"
+    "a9qs9qd9qT5 t5 t5 t5 ",
+    "z9Sx9Dc9S 4do3d 4dt5d",
+    "t5  5d*5  5  5  5 t5 ",
+    " 5d 5d 5d 6+ 7+ 5 C5 ",
+    " 4~ 4~ 4~ 4~ 7+ 4~ 4~",
+    " 5d 5d 5d 6+ 7+ 5 C5 " 
     ]);
 });
