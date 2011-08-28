7c48174f7f3672775549ae90ede6f0a882767d11
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2011-08-28 15:55:26 +0200

    ...

--- js/PlanetCute.js
+++ js/PlanetCute.js
@@ -4,12 +4,10 @@
 //
 
 PlanetCute = function(mapDesc) {
-
-
-    var size = 10;
+    map = [];
 
     function mapParse(mapDesc) {
-        var map, mapXmax, mapYmax, tileset, result, x, y;
+        var tileset, objset, x, y, i;
 
         tileset = {
           "+": "Stone-Block",
@@ -25,6 +23,7 @@ PlanetCute = function(mapDesc) {
           "=": "Wood-Block",
           "#": "Wall-Block"
         };
+
         objset = {
           "t": "Tree-Short",
           "T": "Tree-Tall",
@@ -41,136 +40,107 @@ PlanetCute = function(mapDesc) {
           "c": "Roof-South-East",
         };
 
-        map = {};
-        mapXmax = mapYmax = 0;
-
-        for(y=0;y<mapDesc.length;++y) {
-            for(x=0;x<mapDesc[y].length/3;++x) {
-                var result = {
-                    obj: "" + objset[mapDesc[y][x*3]],
+        for(x=0;x<10;++x) {
+            for(y=0;y<7;++y) {
+                map[y*10+x] = {
+                    item: objset[mapDesc[y][x*3]],
                     h: +mapDesc[y][x*3+1],
-                    tile: "" + tileset[mapDesc[y][x*3+2]],
+                    tile: tileset[mapDesc[y][x*3+2]],
                     x: x,
                     y: y
-                };
-                map[x+','+y] = result;
-                mapXmax = mapXmax < x ? x : mapXmax;
-                mapYmax = mapYmax < y ? y : mapYmax;
-            }
-        }
-
-        return function(x,y) {
-            return map[x+','+y];
-        }
-    }
-    map = mapParse(mapDesc);
-
-    // landscape .4 air .8 floor .8 wall
-    // obj 1.2 air .8 floor
-    var scale = 480/10;
-    var prevx0, prevy0;
-    var imgs;
-    function update(x0, y0, changed) {
-        var x, y, obj, img, style, top, left;
-        if(!imgs) {
-            imgs = [];
-            for(var i=0;i<size*size*2;++i) {
-                var t = $('<img>');
-                t.css('width', scale+1);
-                t.css('height', scale*2+1);
-                $('#sprites').append(t);
-                imgs.push(t[0]);
-            }
-        }
-        if(changed || prevx0 !== x0 || prevy0 !== y0) {
-            prevx0 = x0; prevy0 = y0;
-            for(y=0;y<7;++y) {
-                for(x=0;x<10;++x) {
-                    obj = map(x+x0,y+y0)
-                    img = imgs[x+y*size*2]
-                    img.src = 'imgs/tile/' + obj.tile+ '.png';
-                    top = .8 * scale * y + obj.h * .2 * scale;
-                    left = scale * x;
-                    img.style.left = left + "px";
-                    img.style.top = top + "px";
-                    img.style['z-index'] = y*2;
-
-                    top -= .8 * scale;
-                    img = imgs[x+y*2*size+size]
-                    img.src = 'imgs/obj/' + obj.obj + '.png';
-                    img.style.left = left + "px";
-                    img.style.top = top + "px";
-                    img.style['z-index'] = y*2;
                 }
             }
         }
     }
 
-    function heightAt(x0,y0) {
-        var x = Math.floor(x0);
-        var y = Math.floor(y0);
-        var h0 = map(x,y).h;
-        var h1 = map(x + 1,y).h;
-        var h2 = map(x,y + 1).h;
-        var h3 = map(x + 1,y + 1).h;
-        x = x0 - x;
-        y = y0 - y;
-        return (h0*(1-x) + h1*x)*(1-y) + (h2*(1-x) + h3*x)*y;
+    var scrWidth = $('#viewport').width();
+    var scrHeight = $('#viewport').height();
+    var imWidth = Math.floor(scrWidth / 10);
+    var imHeight = Math.floor(scrHeight / 3.2/ 5) * 5;
+
+    // TILE    OBJ
+    //
+    // air     air
+    // surface air
+    // surface air
+    // pillar  surface
+    // pillar  surface
+
+
+    function drawMap() {
+        var i, html, defaultstyle;
+        html = [];
+        defaultstyle = "width:" + imWidth + "px;";
+        defaultstyle += "height:" + imHeight + "px;";
+        defaultstyle += "position:absolute;"
+        for(i=0;i<map.length;++i) {
+            obj = map[i];
+            if(obj.tile) {
+                console.log(obj.tile); 
+                html.push('<img src="imgs/tile/' + obj.tile + '.png" style="');
+                html.push(defaultstyle);
+                html.push('left:' + imWidth*obj.x + "px;");
+                html.push('z-index:' + obj.y*2 + ";");
+                html.push('top:' + (imHeight*.4*obj.y + (6-obj.h) * imHeight * .1) + "px;");
+                html.push('">');
+            }
+            if(obj.item) {
+                console.log(obj.item); 
+                html.push('<img src="imgs/obj/' + obj.item+ '.png" style="');
+                html.push(defaultstyle);
+                html.push('left:' + imWidth*obj.x + "px;");
+                html.push('z-index:' + obj.y*2 + ";");
+                html.push('top:' + (imHeight*.4*obj.y + (2-obj.h) * imHeight * .1) + "px;");
+                html.push('">');
+            }
+            console.log(obj);
+        }
+        $("#viewport").html(html.join(""));
+    }
+    function imgY(x,y) {
+        return imHeight*.4*y + (2-map[x+10*y].h) * imHeight * .1;
     }
 
-    var imgs2;
-    function drawAt(x,y) {
-        var h = heightAt(x,y);
-        x+=.5;y+=.5;
-        var x0 = Math.floor(x-3);
-        var y0 = Math.floor(y-3);
-        var left = (3 - (x-x0))*scale;
-        var top = (2.5 - (y-y0))*.8*scale -h*.2*scale;
-        //$("#sprites")
-        //    .attr('id', 'sprites2')
-        //    .before($('<div id="sprites"></div>'));
-        //var t = imgs; imgs = imgs2; imgs2 = t;
-        //imgs = undefined;
-        update(x0,y0);
-        $("#sprites").css('left', left);
-        $("#sprites").css('top', top);
-        //$("#sprites2").remove();
-    }
-
-    x = 4; y = 4;
-    function draw() {
-        console.log(x,y);
-        //y+=.01;
-        drawAt(x,y);
-        x+=.10;
-        setTimeout(draw, 100);
-    }
-    update(0,0);
-    $("#sprites").css('top', -30);
-    $("#sprites").css('left');
-    //setTimeout(draw, 2000);
-
-    //drawAt(3.5,3.5);
-    //update(0,0);
+    mapParse(mapDesc);
+    drawMap();
+    $("#viewport").append($('<img src="imgs/obj/Character-Princess-Girl.png" style="position:absolute;top:' + imgY(5,4) + 'px;left:' + 5 * imWidth + 'px;z-index:' + 4*2 + ';width:' + imWidth + 'px;height:' + imHeight + 'px;">'));
 };
 
 Q.setMain(function() {
     PlanetCute([
-    " 4q 4q 4qt4 t4 t4 t4 t4 t4 t4 ",
-    "a0qs0qd0q 4  4  4  4  4  4 t4 ",
-    "z0Sx0Dc0ST4  4  5d 4  4 *4 t4 ",
-    "t4  4d 4  4  4  4 C4  4  4 t4 ",
-    " 4d 4d 4d 3+ 2+ 4  4  4q 4 t4 ",
-    " 4~ 4~ 4~ 4~ 2= 4~ 4~ 4~ 4~ 4~",
-    " 4  4 o4 r4  2+ 3+ 4d 4d 4d 4d"
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 ",
+    " 5  5  5  5  5  5  5  5  5  5 "
     ]);
     PlanetCute([
-    " 4q 4q 4qt4 t4 t4 t4 t4 t4 t4 ",
-    "a0qs0qd0q 4  4  4  4  4  4 t4 ",
-    "z0Sx0Dc0ST4  4  5d 4  4 *4 t4 ",
-    "t4  4d 4  4  4  4 C4  4  4 t4 ",
-    " 4d 4d 4d 3+ 2+ 4  4  4q 4 t4 ",
-    " 4~ 4~ 4~ 4~ 2= 4~ 4~ 4~ 4~ 4~",
-    " 4  4 o4 r4  2+ 3+ 4d 4d 4d 4d"
+    " 9  8  7  6  5  5  5  5  5  5 ",
+    " 7  7  6  6  5  5  7  7  7  7 ",
+    " 6  6  5  5  5  5  7  9  9  7 ",
+    " 5  5  5  5  6  7  8  9  9  7 ",
+    " 4  4  4  5  5  5  7  7  7  7 ",
+    " 2  3  3  4  4  5  5  5  5  5 ",
+    " 0  1  2  3  4  5  5  5  5  5 "
+    ]);
+    PlanetCute([
+    " 9+ 5  5  5  5  5  4d 4d 4d 4d",
+    " 7# 5  5  5  5  5  4d 2o 2o 2o",
+    " 7+ 7+ 7+ 5  5  5  4d 2o 0+ 0+",
+    "o9+ 8+ 7+ 6# 5  4d 3d 2o 1o 0+",
+    " 7+ 7+ 7+ 5  5  5  4d 2o 0+ 0+",
+    " 5  5  5  5  5  5  4d 2o 2o 2o",
+    " 5  5  5  5  5  5  4d 4d 4d 0+"
+    ]);
+    PlanetCute([
+    " 5q 5q 5qt5 t5 t5 t5 t5 t5 t5 ",
+    "a9qs9qd9q 5  5  5  5  5  5 t5 ",
+    "z9Sx9Dc9ST5  5  4d 3d 3d*5 t5 ",
+    "t5  5d 5  5  5  5  5  5  5 t5 ",
+    " 5d 5d 5d 6+ 7+ 5 C5  5q 5 t5 ",
+    " 5~ 5~ 5~ 5~ 7= 5~ 5~ 5~ 5~ 5~",
+    " 5  5 o5 r5  7+ 6+ 5d 5d 5d 5d"
     ]);
 });
